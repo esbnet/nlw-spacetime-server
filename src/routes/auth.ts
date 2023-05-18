f67780798c36c27997b71bb95df10683d70b503f
Edmilson Soares <esbnet@gmail.com>
2023-05-18 18:45:05 -0300

    fix: :bug: fix route 'registre' to 'register'

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,7 +5,7 @@ import { z } from 'zod'
 import { prisma } from '../lib/prisma'
 
 export async function authRoutes(app: FastifyInstance) {
-  app.post('/registre', async (request) => {
+  app.post('/register', async (request) => {
     const bodySchema = z.object({
       code: z.string(),
     })
