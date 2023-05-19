56a930c8903498b672d7eb966010bedbda6359c7
Edmilson Soares <esbnet@gmail.com>
2023-05-19 20:45:06 -0300

    feat: :sparkles: create uplod files route and static file

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,21 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 
 import { memoriesRoutes } from './routes/memories'
-import { usersRoutes } from './routes/users'
+import { uploadRoutes } from './routes/upload'
 import { authRoutes } from './routes/auth'
+import { resolve } from 'path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, { origin: true })
 app.register(jwt, { secret: 'spacetime-app' })
@@ -16,7 +25,7 @@ app.register(jwt, { secret: 'spacetime-app' })
 // Routes
 app.register(authRoutes)
 app.register(memoriesRoutes)
-app.register(usersRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
